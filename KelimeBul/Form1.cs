2e03bc7b2cd8b2488b115e3c2597f90b3d648f84
Enesayyildiz1 <66443194+Enesayyildiz1@users.noreply.github.com>
2020-11-14 21:37:41 +0300

    Add project files.

---
+++ KelimeBul/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KelimeBul
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cumle = textBox1.Text;
+            char harf = Convert.ToChar(textBox2.Text);
+            int toplam = 0;
+            foreach (char i in cumle)
+            {
+                if (i==harf)
+                {
+                    toplam++;
+                }            
+            }
+            label4.Text = toplam.ToString();
+        }
+    }
+}
